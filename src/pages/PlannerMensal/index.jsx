6b1de24f7ff9ed560f45bdd80c038c0ae3527b51
thabatafrs <thabatafreitas10@gmail.com>
2025-05-08 11:09:59 -0300

    adiciona lógica de criar eventos

--- src/pages/PlannerMensal/index.jsx
+++ src/pages/PlannerMensal/index.jsx
@@ -1,11 +1,52 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
 
 function PlannerMensal({ mes }) {
   const [semanasDoMes, setSemanasDoMes] = useState([]);
   const [hoje] = useState(new Date());
+  const [mostrarPopup, setMostrarPopup] = useState(false);
+  const [diaSelecionado, setDiaSelecionado] = useState(null);
+  const [diasSemanaSelecionados, setDiasSemanaSelecionados] = useState([]);
+
+  const inputNomeEvento = useRef();
+  const inputHorarioEvento = useRef();
+
+  async function createEvento() {
+    const nome = inputNomeEvento.current.value.trim();
+    const horario = inputHorarioEvento.current.value.trim();
+
+    if (!nome || !horario) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    try {
+      await api.post("/eventos", {
+        nome,
+        horario,
+        data: diaSelecionado.toISOString(), // campo certo pro schema
+      });
+      alert("Evento cadastrado com sucesso!");
+      setMostrarPopup(false);
+    } catch (error) {
+      alert(
+        "Erro ao cadastrar usuário: " + error.response?.data?.message ||
+          error.message
+      );
+    }
+  }
+
+  const [isHabito, setIsHabito] = useState(true);
+
+  function abrirPopup(dia) {
+    setDiaSelecionado(dia);
+    setMostrarPopup(true);
+  }
 
   const navigate = useNavigate();
+
+  const diasDaSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
 
   const nomesMeses = [
     "janeiro",
@@ -85,7 +126,7 @@ function PlannerMensal({ mes }) {
             {semana.map((dia, index) => (
               <div
                 key={index}
-                className={`border p-4 text-center rounded h-24 ${
+                className={`border p-4 text-center rounded h-24 flex justify-between ${
                   dia &&
                   dia.getDate() === hoje.getDate() &&
                   dia.getMonth() === hoje.getMonth() &&
@@ -95,11 +136,152 @@ function PlannerMensal({ mes }) {
                 }`}
               >
                 {dia ? dia.getDate() : ""}
+                <button
+                  type="button"
+                  className="bg-green-500 text-white rounded-sm h-5 flex items-center cursor-pointer"
+                  onClick={() => abrirPopup(dia)}
+                >
+                  +
+                </button>
               </div>
             ))}
           </div>
         ))}
       </div>
+
+      {mostrarPopup && (
+        <div className="fixed inset-0 bg-gray-100/75 flex items-center justify-center z-50">
+          <div className="bg-white rounded-3xl p-6 w-96 text-center">
+            {diaSelecionado && (
+              <p className="text-gray-600 mb-3">
+                Dia selecionado: {diaSelecionado.toLocaleDateString("pt-BR")}
+              </p>
+            )}
+            <div className="flex justify-center gap-3  mb-5 ">
+              <button
+                className={`font-semibold uppercase ${
+                  isHabito
+                    ? "underline text-black"
+                    : "text-gray-600 hover:underline"
+                }`}
+                onClick={() => setIsHabito(true)}
+              >
+                Hábito
+              </button>
+              <button
+                className={`font-semibold uppercase ${
+                  !isHabito
+                    ? "underline text-black"
+                    : "text-gray-600 hover:underline"
+                }`}
+                onClick={() => setIsHabito(false)}
+              >
+                Evento
+              </button>
+            </div>
+
+            <div>
+              {isHabito ? (
+                <div className="flex flex-col gap-3">
+                  <div className="flex items-center gap-3 ">
+                    <label htmlFor="">Nome:</label>
+                    <input
+                      className="border p-2 rounded w-full"
+                      placeholder=""
+                    />
+                  </div>
+
+                  <div className="flex items-center gap-3">
+                    <label htmlFor="">Horário:</label>
+                    <input
+                      className="border p-2 rounded"
+                      placeholder=""
+                      type="time"
+                    />
+                  </div>
+
+                  <div>
+                    <label className="block text-left mb-1 font-semibold">
+                      Repetir em:
+                    </label>
+                    <div className="grid grid-cols-4 gap-2 text-sm">
+                      {diasDaSemana.map((dia, index) => (
+                        <label key={index} className="flex items-center gap-1">
+                          <input
+                            type="checkbox"
+                            value={dia}
+                            checked={diasSemanaSelecionados.includes(dia)}
+                            onChange={(e) => {
+                              const { value, checked } = e.target;
+                              if (checked) {
+                                setDiasSemanaSelecionados([
+                                  ...diasSemanaSelecionados,
+                                  value,
+                                ]);
+                              } else {
+                                setDiasSemanaSelecionados(
+                                  diasSemanaSelecionados.filter(
+                                    (d) => d !== value
+                                  )
+                                );
+                              }
+                            }}
+                          />
+                          {dia}
+                        </label>
+                      ))}
+                    </div>
+                  </div>
+
+                  <button className="bg-green-500 text-white px-4 py-2 mt-4 rounded w-full ">
+                    salvar
+                  </button>
+                  <button
+                    onClick={() => setMostrarPopup(false)}
+                    className="text-sm text-red-500 hover:underline"
+                  >
+                    cancelar
+                  </button>
+                </div>
+              ) : (
+                <div className="flex flex-col gap-3">
+                  <div className="flex items-center gap-3 ">
+                    <label htmlFor="">Nome:</label>
+                    <input
+                      className="border p-2 rounded w-full"
+                      placeholder=""
+                      ref={inputNomeEvento}
+                    />
+                  </div>
+
+                  <div className="flex items-center gap-3">
+                    <label htmlFor="">Horário:</label>
+                    <input
+                      className="border p-2 rounded"
+                      placeholder=""
+                      type="time"
+                      ref={inputHorarioEvento}
+                    />
+                  </div>
+
+                  <button
+                    className="bg-green-500 text-white px-4 py-2 mt-4 rounded w-full "
+                    onClick={createEvento}
+                  >
+                    salvar
+                  </button>
+                  <button
+                    onClick={() => setMostrarPopup(false)}
+                    className="text-sm text-red-500 hover:underline"
+                  >
+                    cancelar
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
